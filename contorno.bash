03c27b769cd196cd78b77dc198413d144e82d73b
arthurfelixgr <arthurfelixgr@gmail.com>
2023-02-10 18:27:22 -0300

    Add files via upload

---
+++ contorno.bash
@@ -0,0 +1,54 @@
+#!/bin/bash -
+
+mkdir sectores
+i=1
+primeira=''
+
+grep -v "\(SETOR\|MODIFICADO\|LAT[[:blank:]]\{1,\}LON\)" < "$1" | sed -e '1{/^$/d}' -e '$a \\' | while read linha
+do 
+    arquivo=setor$(printf "%.2d" "$i")
+
+    echo "$linha" | grep '^[[:space:]]*$' > /dev/null  && {
+        printf '\n' >> sectores/$arquivo
+        ((i++))
+        continue
+    }
+
+    echo "$linha" >> sectores/$arquivo
+done 
+
+cat > contorno.py <<EOF
+#!/bin/python3
+
+from shapely.geometry import Polygon
+from shapely.ops import unary_union
+
+EOF
+
+sects=
+cd sectores 
+
+for i in *
+do 
+    echo "$i" | grep 'setor[0-9][0-9]' > /dev/null && {
+        echo $( cut --complement -f1 < "$i" | sed -e '$d' -e 's/^/(/' -e 's/$/), /' -e 's/\t/, /' | tr '\n' ' ' | sed -e "s/^/$i = Polygon([/" -e "s/, *$/])/" ) >> ../contorno.py 
+        printf '\n' >> ../contorno.py 
+        sects+=("$i")
+    }
+done 
+
+cd ..
+rm -r sectores
+echo "fir = unary_union([$( echo "${sects[@]}" | sed -e 's/ /, /g' -e 's/^, //' )])" >> contorno.py 
+echo "print(fir)" >> contorno.py 
+chmod +x contorno.py
+
+./contorno.py | sed -e 's/^.*((//' -e 's/))$//' | tr ',' '\n' | sed -e 's/^[[:space:]]*//' -e 's/ /\t/' | while read linha 
+do 
+    linha=$(echo "$linha" | sed 's/[[:space:]]\{1,\}/\t/g' )
+    latitude=$( echo "$linha" | cut -f1 )
+    longitude=$( echo "$linha" | cut -f2 )
+
+    grep -m1 "\\$latitude[[:blank:]]\{1,\}$longitude" "$1"
+done 
+rm contorno.py
---
+++ setores-cartesianos.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while read linha
+do 
+    if echo "$linha" | grep -v "\(SETOR\|LAT[[:blank:]]\{1,\}LON\|^[[:blank:]]*$\|MODIFICADO\)" > /dev/null
+    then 
+        fixo=$(echo "$linha" | cut -f1)
+        coord=$(echo "$linha" | cut --complement -f1 | sed 's/\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\([S,N]\)[[:blank:]]\([0-9]\{3\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\([W,E]\)/\4\t\1\t\2\t\3\t\8\t\5\t\6\t\7\t/')
+        
+        hemx=$(echo "$coord" | cut -f1)
+        sigx=''
+        graux=$(( 10#$(echo "$coord" | cut -f2) ))
+        minx=$(( 10#$(echo "$coord" | cut -f3) ))
+        segx=$(( 10#$(echo "$coord" | cut -f4) ))
+
+        hemy=$(echo "$coord" | cut -f5)
+        sigy=''
+        grauy=$(( 10#$(echo "$coord" | cut -f6) ))
+        miny=$(( 10#$(echo "$coord" | cut -f7) ))
+        segy=$(( 10#$(echo "$coord" | cut -f8) ))
+
+        [ "$hemx" = 'S' ] && sigx='-'
+        [ "$hemy" = 'W' ] && sigy='-'
+
+        x=$(echo "$sigx")$(awk "BEGIN{ OFMT=\"%0.14f\"; print $graux + $minx/60 + $segx/3600 ; }")
+        y=$(echo "$sigy")$(awk "BEGIN{ OFMT=\"%0.14f\"; print $grauy + $miny/60 + $segy/3600 ; }")
+
+        printf "$fixo\t$x\t$y\n" | sed 's/0*$//'
+    else 
+        echo "$linha"
+    fi 
+done < "$1"
