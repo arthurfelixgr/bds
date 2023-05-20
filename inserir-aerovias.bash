8b3b088f66238682bad5d12d48741a97fa614f19
arthurfelixgr <arthurfelixgr@gmail.com>
2023-05-20 18:30:36 -0300

    Update inserir-aerovias.bash

--- inserir-aerovias.bash
+++ inserir-aerovias.bash
@@ -33,6 +33,99 @@ else
    exit 1
 fi 
 
+cat <<_EOF > pointinpoly.js # créditos: https://github.com/metafloor/pointinpoly
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        let exports = factory();
+        root.pointInPoly = exports.pointInPoly;
+        root.pointInXYPoly = exports.pointInXYPoly;
+    }
+}(typeof self !== 'undefined' ? self : this, function () {
+
+function ptinxypoly(x, y, poly) {
+    let c = false;
+    for (let l = poly.length, i = 0, j = l-1; i < l; j = i++) {
+        let xj = poly[j].x, yj = poly[j].y, xi = poly[i].x, yi = poly[i].y;
+        let where = (yi - yj) * (x - xi) - (xi - xj) * (y - yi);
+        if (yj < yi) {
+            if (y >= yj && y < yi) {
+                if (where == 0) return true; 
+                if (where > 0) {
+                    if (y == yj) { 
+                        if (y > poly[j == 0 ? l-1 : j-1].y) {
+                            c = !c;
+                        }
+                    } else {
+                        c = !c;
+                    }
+                }
+            }
+        } else if (yi < yj) {
+            if (y > yi && y <= yj) {
+                if (where == 0) return true; 
+                if (where < 0) {
+                    if (y == yj) { 
+                        if (y < poly[j == 0 ? l-1 : j-1].y) {
+                            c = !c;
+                        }
+                    } else {
+                        c = !c;
+                    }
+                }
+            }
+        } else if (y == yi && (x >= xj && x <= xi || x >= xi && x <= xj)) {
+            return true; 
+        }
+    }
+    return c;
+}
+
+function ptinpoly(x, y, poly) {
+    let c = false;
+    for (let l = poly.length, i = 0, j = l-1; i < l; j = i++) {
+        let xj = poly[j][0], yj = poly[j][1], xi = poly[i][0], yi = poly[i][1];
+        let where = (yi - yj) * (x - xi) - (xi - xj) * (y - yi);
+        if (yj < yi) {
+            if (y >= yj && y < yi) {
+                if (where == 0) return true; 
+                if (where > 0) {
+                    if (y == yj) { 
+                        if (y > poly[j == 0 ? l-1 : j-1][1]) {
+                            c = !c;
+                        }
+                    } else {
+                        c = !c;
+                    }
+                }
+            }
+        } else if (yi < yj) {
+            if (y > yi && y <= yj) {
+                if (where == 0) return true; 
+                if (where < 0) {
+                    if (y == yj) { 
+                        if (y < poly[j == 0 ? l-1 : j-1][1]) {
+                            c = !c;
+                        }
+                    } else {
+                        c = !c;
+                    }
+                }
+            }
+        } else if (y == yi && (x >= xj && x <= xi || x >= xi && x <= xj)) {
+            return true; 
+        }
+    }
+    return c;
+}
+
+return { pointInPoly:ptinpoly, pointInXYPoly:ptinxypoly };
+}));
+_EOF
+
 planilha="$1"
 base="$2"
 nomeBase=$(tar -tf "$base" | head -n1 | cut -d'_' -f1)
@@ -103,10 +196,26 @@ extrairContorno() {
       echo "$linha" | cartesianas | sed -e 's/^ */[/' -e 's/ *$/], /' -e 's/\([0-9]\)  *\([-0-9]\)/\1, \2/'
    done 
 
+   test -n "$pasta" && grep '^SBRE' "$pasta/firseg_data" | 
+   head -n1 |  
+   awk -F';' '{ printf "%s%02d%02d%02d %s%03d%02d%02d\n", $5, $6, $7, $8, $9, $10, $11, $12 }' | 
+   cartesianas | 
+   sed -e 's/^ */[/' -e 's/ *$/], /' -e 's/\([0-9]\)  *\([-0-9]\)/\1, \2/'
+
    printf "]"
 }
 
-#pontoDentro() {}
+pontoDentro() {
+   coords=$(cartesianas "$1" "$2")
+   lat=$(echo "$coords" | awk '{ print $1 }')
+   lon=$(echo "$coords" | awk '{ print $2 }')
+   fir=$(extrairContorno)
+
+   cat <<_EOF | node
+   var pointIn = require('./pointinpoly').pointInPoly;
+   console.log(pointIn($lat, $lon, $fir));
+_EOF
+}
 
 atualizarBalizas() {
    echo "Atualizando balizas..." >&2
@@ -164,3 +273,6 @@ atualizarBalizas() {
 
    echo "Balizas atualizadas. " >&2
 }
+
+extrairBase "$base"
+pontoDentro S125431 W0381921
